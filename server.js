f6ad6b58f8d1182e226694bfe342520ab66fd955
Mateusz Stomski <lehronn@gmail.com>
2017-11-10 10:03:10 +0100

    exercise is complete

--- server.js
+++ server.js
@@ -7,35 +7,19 @@ var app = express();
 app.set('view engine', 'pug');  //kreator widoków.
 app.set('views','./views');     //widoki będą w katalogu /views
 
-app.get('/', function (req, res) {
-    res.send('Hello world!');
+//serwowanie plików statycznych z katalogu assets.
+app.use('/static', express.static("assets"));
+
+app.get('/', function(req, res) {
+  res.render('index',
+    {
+      user: { name: "Johnny", age: "20" }
+    }
+  );
 });
 
-app.use('/store', function(req, res, next){
-    console.log('Jestem pośrednikiem przy żądaniu do /store');
-    next();
-});
-
-app.get('/store', function (req, res) {
-    res.send('To jest sklep');
-});
-
-//na tej stronie będzie przekazana wartość zmiennych name i url.
-app.get('/dynamic-view', function(req, res){
-    res.render('dynamic', {
-        name: "Moja dynamiczna strona",
-        url: "http://www.google.com",
-        user: { name: "Johnny", age: "20" }
-    });
-});
-
-app.get('/first-template', function(req, res) {
-  res.render('first-template');
-});
-
-//prezentacja jak wygląda includowanie pugów wewnątrz siebie:
-app.get('/includingPugs', function(req, res) {
-  res.render('main');
+app.get('/auth/google', function(req, res) {
+  res.render('inside');
 });
 
 //nasłuchiwanie na porcie i obsługa błędu 404.
